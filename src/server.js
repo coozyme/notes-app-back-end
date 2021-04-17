4a50c03fb5d6b9ed18946892c07c9d5b2759e2cf
Ary Setya Pambudi <programmeranak@gmail.com>
2021-04-17 21:21:19 +0700

    update server.js

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.env.Node_ENV !== 'production' ? 'localhost' : '172.31.37.37' ,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.37.37' ,
     routes: {
       cors: {
         origin: ['*'],
